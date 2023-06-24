8d23313d2bf6b980a756a411d3fd3f03dda6998d
ssyssoev <ssyssoev@mail.ru>
2023-06-24 20:16:09 +0300

    Исправлены замечания

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("PrintOut HomeWork7");
+        System.out.println("PrintOut HomeWork7. Corrected");
 
 
         Task1();
